11a89a2d1b72bfb5509b3ec6b98126cf9d3f4dc0
Edwin Cheung <eccheung4@gmail.com>
2017-03-28 18:49:51 -0700

    fix: update to send openrtb bidRequest to endpoint

--- src/adapters/vidRoll.js
+++ src/adapters/vidRoll.js
@@ -5,12 +5,7 @@ import * as utils from 'src/utils';
 import { ajax } from 'src/ajax';
 import { STATUS } from 'src/constants';
 
-const ENDPOINT = '//hollywoodwire.tv/vidrollBidder.php';
-const VIDEO_TARGETING = ['id', 'mimes', 'minduration', 'maxduration',
-  'startdelay', 'skippable', 'playback_method', 'frameworks'];
-const USER_PARAMS = [
-  'age', 'external_uid', 'segments', 'gender', 'dnt', 'language'
-];
+const ENDPOINT = '//hollywoodwire.tv/rtbBidder.php';
 
 /**
  * Bidder adapter for /ut endpoint. Given the list of all ad unit tag IDs,
@@ -23,80 +18,69 @@ function VidRollAdapter() {
   let bidRequests = {};
   let usersync = false;
 
+  let  openrtbBidRequest = {
+    "at": 2,          // auction type
+    "tmax": 120,      // timeout max
+    "imp": [],
+    "site": {
+      "name": window.top.document.title,     // Site name
+      "domain": utils.getTopWindowLocation().hostname,
+      "page": utils.getTopWindowUrl(),
+      "ref": document.referrer,
+    },
+    "device": {
+      "ip": "",
+      "ua": window.navigator.userAgent,
+      "os": "",
+      "dnt": 0          // 1 - do not track
+    }
+  };
+
+  function createImpObj(id, w, h, bidfloor) {
+    var obj = {};
+
+    obj.id = id;
+    obj.bidfloor = bidfloor;
+    obj.video = {
+      w,
+      h,
+      "pos": 1,
+      "api": [1, 2],
+      "protocols": [2, 3],
+      "mimes": [
+        "video/mp4"
+      ],
+      "linearity": 1
+    }
+    return obj;
+  }
+
   /* Prebid executes this function when the page asks to send out bid requests */
   baseAdapter.callBids = function(bidRequest) {
     const bids = bidRequest.bids || [];
     var member = 0;
     let userObj;
     console.log(`callBids`, bids);
-    const tags = bids
+    const imps = bids
       .filter(bid => valid(bid))
       .map(bid => {
         // map request id to bid object to retrieve adUnit code in callback
         bidRequests[bid.bidId] = bid;
 
-        let tag = {};
-        tag.sizes = getSizes(bid.sizes);
-        tag.primary_size = tag.sizes[0];
-        tag.uuid = bid.bidId;
-        if(bid.params.placementId) {
-          tag.id = parseInt(bid.params.placementId, 10);
-        } else {
-          tag.code = bid.params.invCode;
-        }
-        tag.allow_smaller_sizes = bid.params.allowSmallerSizes || false;
-        tag.prebid = true;
-        tag.disable_psa = true;
-        member = parseInt(bid.params.member, 10);
-        if (bid.params.reserve) {
-          tag.reserve = bid.params.reserve;
-        }
-        if (bid.params.position) {
-          tag.position = {'above': 1, 'below': 2}[bid.params.position] || 0;
-        }
-        if (bid.params.trafficSourceCode) {
-          tag.traffic_source_code = bid.params.trafficSourceCode;
-        }
-        if (bid.params.privateSizes) {
-          tag.private_sizes = getSizes(bid.params.privateSizes);
-        }
-        if (bid.params.supplyType) {
-          tag.supply_type = bid.params.supplyType;
-        }
-        if (bid.params.pubClick) {
-          tag.pubclick = bid.params.pubClick;
-        }
-        if (bid.params.extInvCode) {
-          tag.ext_inv_code = bid.params.extInvCode;
-        }
-        if (bid.params.externalImpId) {
-          tag.external_imp_id = bid.params.externalImpId;
-        }
-        if (!utils.isEmpty(bid.params.keywords)) {
-          tag.keywords = getKeywords(bid.params.keywords);
-        }
+        openrtbBidRequest.id = bid.bidId;
 
-        if (bid.mediaType === 'video') {tag.require_asset_url = true;}
-        if (bid.params.video) {
-          tag.video = {};
-          // place any valid video params on the tag
-          Object.keys(bid.params.video)
-            .filter(param => VIDEO_TARGETING.includes(param))
-            .forEach(param => tag.video[param] = bid.params.video[param]);
-        }
+        const params = bid.params;
+        const dim = bid.sizes;
 
-        if (bid.params.user) {
-          userObj = {};
-          Object.keys(bid.params.user)
-            .filter(param => USER_PARAMS.includes(param))
-            .forEach(param => userObj[param] = bid.params.user[param]);
-        }
+        let imp = createImpObj(parseInt(params.placementId), dim[0], dim[1], params.bidFloor || 0);
 
-        return tag;
+        return imp;
       });
 
-    if (!utils.isEmpty(tags)) {
-      const payloadJson = {tags: [...tags], user: userObj};
+    if (!utils.isEmpty(imps)) {
+      openrtbBidRequest.imp = imps;
+      console.log(openrtbBidRequest)
+      const payloadJson = openrtbBidRequest;
       if (member > 0) {
         payloadJson.member_id = member;
       }
@@ -130,44 +114,27 @@ function VidRollAdapter() {
       Object.keys(bidRequests)
         .map(bidId => bidRequests[bidId].placementCode)
         .forEach(placementCode => {
-          bidmanager.addBidResponse(placementCode, createBid(STATUS.NO_BID));
+          bidmanager.addBidResponse(placementCode, bidfactory.createBid(STATUS.NO_BID));
         });
       return;
     }
 
-    parsed.tags.forEach(tag => {
-      const ad = getRtbBid(tag);
-      const cpm = ad && ad.cpm;
-      const type = ad && ad.ad_type;
+    utils._each(parsed.seatbid, function(seatbid) {
+      utils._each(seatbid.bid, function(seatbidBid) {
+        let bid = bidfactory.createBid(STATUS.GOOD);
 
-      let status;
-      if (cpm !== 0 && (type === 'banner' || type === 'video')) {
-        status = STATUS.GOOD;
-      } else {
-        status = STATUS.NO_BID;
-      }
-
-      if (type && (type !== 'banner' && type !== 'video')) {
-        utils.logError(`${type} ad type not supported`);
-      }
-
-      tag.bidId = tag.uuid;  // bidfactory looks for bidId on requested bid
-      const bid = createBid(status, tag);
-      if (type === 'video') bid.mediaType = 'video';
-      const placement = bidRequests[bid.adId].placementCode;
-      bidmanager.addBidResponse(placement, bid);
+        bid.code = baseAdapter.getBidderCode();
+        bid.bidderCode = baseAdapter.getBidderCode();
+        bid.cpm = seatbidBid.price;
+        bid.vastUrl = seatbidBid.nurl;
+        bid.descriptionUrl = seatbidBid.nurl;
+        bid.creative_id = seatbidBid.crid;
+        bid.width = seatbidBid.w;
+        bid.height = seatbidBid.h;
+        console.log(`addBidResponse`, bidRequests[parsed.id].placementCode, bid);
+        bidmanager.addBidResponse(bidRequests[parsed.id].placementCode, bid);
+      });
     });
-
-    if (!usersync) {
-      const iframe = utils.createInvisibleIframe();
-      iframe.src = '//acdn.adnxs.com/ib/static/usersync/v3/async_usersync.html';
-      try {
-        document.body.appendChild(iframe);
-      } catch (error) {
-        utils.logError(error);
-      }
-      usersync = true;
-    }
   }
 
   /* Check that a bid has required paramters */
@@ -177,89 +144,6 @@ function VidRollAdapter() {
     } else {
       utils.logError('bid requires placementId or (member and invCode) params');
     }
-  }
-
-  /* Turn keywords parameter into ut-compatible format */
-  function getKeywords(keywords) {
-    let arrs = [];
-
-    utils._each(keywords, (v, k) => {
-      if (utils.isArray(v)) {
-        let values = [];
-        utils._each(v, (val) => {
-          val = utils.getValueString('keywords.' + k, val);
-          if (val) {values.push(val);}
-        });
-        v = values;
-      } else {
-        v = utils.getValueString('keywords.' + k, v);
-        if (utils.isStr(v)) {v = [v];}
-        else {return;} // unsuported types - don't send a key
-      }
-      arrs.push({key: k, value: v});
-    });
-
-    return arrs;
-  }
-
-  /* Turn bid request sizes into ut-compatible format */
-  function getSizes(requestSizes) {
-    let sizes = [];
-    let sizeObj = {};
-
-    if (utils.isArray(requestSizes) && requestSizes.length === 2 &&
-       !utils.isArray(requestSizes[0])) {
-      sizeObj.width = parseInt(requestSizes[0], 10);
-      sizeObj.height = parseInt(requestSizes[1], 10);
-      sizes.push(sizeObj);
-    } else if (typeof requestSizes === 'object') {
-      for (let i = 0; i < requestSizes.length; i++) {
-        let size = requestSizes[i];
-        sizeObj = {};
-        sizeObj.width = parseInt(size[0], 10);
-        sizeObj.height = parseInt(size[1], 10);
-        sizes.push(sizeObj);
-      }
-    }
-
-    return sizes;
-  }
-
-  function getRtbBid(tag) {
-    return tag && tag.ads && tag.ads.length && tag.ads.find(ad => ad.rtb);
-  }
-
-  /* Create and return a bid object based on status and tag */
-  function createBid(status, tag) {
-    const ad = getRtbBid(tag);
-    let bid = bidfactory.createBid(status, tag);
-    bid.code = baseAdapter.getBidderCode();
-    bid.bidderCode = baseAdapter.getBidderCode();
-
-    if (ad && status === STATUS.GOOD) {
-      bid.cpm = ad.cpm;
-      bid.creative_id = ad.creative_id;
-
-      if (ad.rtb.video) {
-        bid.width = ad.rtb.video.player_width;
-        bid.height = ad.rtb.video.player_height;
-        bid.vastUrl = ad.rtb.video.asset_url;
-        bid.descriptionUrl = ad.rtb.video.asset_url;
-      } else {
-        bid.width = ad.rtb.banner.width;
-        bid.height = ad.rtb.banner.height;
-        bid.ad = ad.rtb.banner.content;
-        try {
-          const url = ad.rtb.trackers[0].impression_urls[0];
-          const tracker = utils.createTrackPixelHtml(url);
-          bid.ad += tracker;
-        } catch (error) {
-          utils.logError('Error appending tracking pixel', error);
-        }
-      }
-    }
-
-    return bid;
   }
 
   return {
